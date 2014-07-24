26674f72419b5c8357b06f1915210793b752ef62
garciadanny <dannygarcia.me@gmail.com>
2014-07-24 15:32:29 -0600

    add socket.io
    Initialize an instance of an `http` server and pass the `app` function
    as a callback to handle requests. Then, initialize an instance of
    socket.io by passing it the `http` server object. Listen for
    `connection` events.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var http = require('http');
+var socket = require('socket.io');
+
 var app = express();
+var server = http.Server(app);
+var io = socket.listen(server);
 
 app.use(express.static(__dirname + '/app/views'));
 app.use(express.static(__dirname + '/app/assets'));
@@ -8,6 +13,13 @@ app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
 
-app.listen(8080, function() {
+io.on('connection', function(socket) {
+   console.log('A user connected...');
+   socket.on('disconnect', function() {
+       console.log('A user disconnected...');
+   })
+});
+
+server.listen(8080, function() {
     console.log('Listening on port: 8080')
 });
