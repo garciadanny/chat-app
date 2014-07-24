461caf668e13f4e896e2b3d34fca87fc5d6de6df
garciadanny <dannygarcia.me@gmail.com>
2014-07-24 15:07:30 -0600

    add middleware for static files

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/app/views'));
+app.use(express.static(__dirname + '/app/assets'));
+
 app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
