4311eec4988b25fe560a57a30625ddcd447dc4a4
garciadanny <dannygarcia.me@gmail.com>
2014-07-25 16:49:58 -0600

    Persist message history & add username support

--- index.js
+++ index.js
@@ -1,30 +1,71 @@
 var express = require('express');
 var http = require('http');
 var socket = require('socket.io');
+var redis = require('redis');
+
 
 var app = express();
 var server = http.Server(app);
 var io = socket.listen(server);
+var redisClient = redis.createClient();
 
 app.use(express.static(__dirname + '/app/views'));
 app.use(express.static(__dirname + '/app/assets'));
 
-app.get('/', function(req, res) {
-    res.sendfile('index.html');
+app.get('/', function (req, res) {
+  res.sendfile('index.html');
 });
 
-io.on('connection', function(socket) {
-   console.log('A user connected...');
+io.on('connection', function (socket) {
+  console.log('A user connected...');
 
-   socket.on('chat message', function(message) {
-    socket.broadcast.emit('chat message', message);
-   });
+  var socketId = socket.id.toString();
 
-   socket.on('disconnect', function() {
-       console.log('A user disconnected...');
-   });
+  socket.on('join', function (userName) {
+    redisClient.set(socketId, userName);
+    socket.broadcast.emit('join', userName);
+
+    redisClient.lrange("messages", 0, -1, function(err, messages) {
+      if (err) throw err
+      messages = messages.reverse();
+
+      messages.forEach(function(message) {
+        try {
+          var msg = JSON.parse(message)
+          socket.emit("chat message", msg.userName, msg.data);
+        } catch (err) {
+          console.error(err)
+          console.log('not a valid message:', message)
+        }
+      });
+    });
+  });
+
+  socket.on('chat message', function (message) {
+
+    redisClient.get(socketId, function(err, userName) {
+      if (err) throw err
+      socket.broadcast.emit('chat message', userName, message);
+
+      var userMessage = JSON.stringify( {userName: userName, data: message} );
+      redisClient.lpush("messages", userMessage, function(err, userMessage) {
+        redisClient.ltrim("messages", 0, 20);
+      });
+    });
+  });
+
+  socket.on('disconnect', function () {
+    console.log('A user disconnected...');
+
+    redisClient.get(socketId, function(err, userName) {
+      if (err) throw err
+      socket.broadcast.emit('disconnected', userName);
+      redisClient.del(socketId);
+//      redisClient.del("messages");
+    });
+  });
 });
 
-server.listen(8080, function() {
-    console.log('Listening on port: 8080')
+server.listen(8080, function () {
+  console.log('Listening on port: 8080')
 });
