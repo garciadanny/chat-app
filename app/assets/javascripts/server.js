1451eeccd52c783d558b4db0d403321d3d9ae664
garciadanny <dannygarcia.me@gmail.com>
2014-07-28 15:31:16 -0600

    change redis client variable in prod to redisClient

--- app/assets/javascripts/server.js
+++ app/assets/javascripts/server.js
@@ -5,8 +5,8 @@
 
 if (process.env.REDISTOGO_URL) {
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var redis = require("redis").createClient(rtg.port, rtg.hostname);
-  redis.auth(rtg.auth.split(":")[1]);
+  var redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+  redisClient.auth(rtg.auth.split(":")[1]);
 } else {
   var redisClient = require('redis').createClient();
 }
