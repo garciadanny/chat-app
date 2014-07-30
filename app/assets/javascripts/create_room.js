6abdb7d9364e7c841094614a3aeb9ec3aac2375c
garciadanny <dannygarcia.me@gmail.com>
2014-07-30 13:48:00 -0600

    remove comments and handling of form submission

--- app/assets/javascripts/create_room.js
+++ app/assets/javascripts/create_room.js
@@ -11,23 +11,4 @@ $(document).ready( function() {
     $(".actions").find(".action-button").hide();
     $("#join-form").fadeToggle();
   });
-
-  $("#create-form").find("form").submit( function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    var username = $("#create-username").val();
-    var chatroom = $("#create-chatroom").val();
-
-//    socket.emit("initialize room", username, chatroom);
-  });
-
-//  $(".brand").on("click", function(event) {
-//    event.preventDefault();
-//    event.stopPropagation();
-////    $("form:visible").css("display", "none");
-//    $("form:visible").hide( function() {
-//      $(".actions").find(".action-button").show();
-//    });
-//  });
 });
